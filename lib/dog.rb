dbde71524dee38f5f5b176e1f07761748f50be3a
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-05 00:00:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,14 @@ class Dog
   def sit 
     puts "The Dog is sitting"
   end
+end
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
   
+  def walk
+    puts "The Person is walking"
+  end
+end
